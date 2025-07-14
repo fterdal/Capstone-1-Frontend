90ac2ba7e3e8095f4f9ddcc2d1d86df92458cc7c
Tran Vo <vobaohoangtran@gmail.com>
2025-07-13 21:42:59 -0400

    feat: added 'Get Started' button that prompts user to sign up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
+
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>Welcome to Rankzilla</h1>
       <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
+      <button onClick={() => navigate("/login")}>Get Started</button>
     </>
   );
 };
