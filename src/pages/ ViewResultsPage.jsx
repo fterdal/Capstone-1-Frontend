5573ac486953c1d9c3221d68dc41a814dab872e9
hailia sommerville <hailiassommerville@gmail.com>
2025-07-14 14:33:47 -0400

    feat: routing added and display message

--- src/pages/ ViewResultsPage.jsx
+++ src/pages/ ViewResultsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const ViewResultsPage = () => {
+const ViewResultsPage = ({rankings,poll}) => {
 
     return (
         <div className="view-results-page">
