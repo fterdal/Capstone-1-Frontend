9b3cef0c16fbb56a6b8ff749751cbb449f81420e
Tran Vo <vobaohoangtran@gmail.com>
2025-07-13 21:34:08 -0400

    chore: added CSS file for Home.jsx and minor addition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./Home.css";
 const Home = () => {
   return (
     <>
-      <h1>Hello React!</h1>
+      <h1>Welcome to Rankzilla</h1>
       <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
     </>
   );
