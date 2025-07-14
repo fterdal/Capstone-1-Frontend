d041090ef1238a0c056909d5d5fdb40f0cc9bea5
Tran Vo <vobaohoangtran@gmail.com>
2025-07-13 23:07:31 -0400

    fix/style: added a div and minor styling changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,9 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div classname ="home-container">
+    <div className ="home-container">
       <h1>Welcome to Rankzilla</h1>
       <p>Rankzilla is a ranking poll platform that lets users vote with more nuance.Instead of just choosing one option, people rank choices, and Rankzilla figures out the winner—fairly.</p>
-      <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
       <button onClick={() => navigate("/login")}>Get Started</button>
 
     </div>
