4a98efc8c26d2aa7eb87b613b98932d90f5d312a
hailia sommerville <hailiassommerville@gmail.com>
2025-07-14 15:45:24 -0400

    feat: create new poll btn added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+    const navigate = useNavigate();
+    
     return (
         <div>
-            
+            <h3>This is the dashboard/new homepage to view all polls</h3>
+
+            <button onClick={navigate("/polls/create")}>Create New Poll</button>
         </div>
     );
 }
