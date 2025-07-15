d5d49bdfe52e741c4a94efd8d876c8f449fd4512
Bar Yaakov <baryaakov5050@gmail.com>
2025-07-15 14:04:54 -0400

    test

---
+++ src/components/poll.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../shared";
+
+const Poll = ({ poll }) => {
+
+  return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  )
+};
+
+export default Poll;
