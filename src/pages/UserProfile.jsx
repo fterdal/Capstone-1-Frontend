36e7c5396ad089ab6596f3bdd86f74df148abd17
hailia sommerville <hailiassommerville@gmail.com>
2025-07-24 02:56:20 -0400

    chore: User Profile created

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserProfile = () => {
+  return (
+    <div className="user-profile">
+      <h2>User Profile</h2>
+      <div className="profile-info">
+      </div>
+    </div>
+  );
+}
+export default UserProfile;
